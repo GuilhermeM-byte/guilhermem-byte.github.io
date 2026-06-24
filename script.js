47ccdc8605c1aaac7d860418f01016b57e372552
Guilherme Medeiros <gmemedeiros496@gmail.com>
2026-06-24 20:13:29 +0100

    atualizacao 24062026

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 function translatePage(lang) {
     const pageLang = document.documentElement.lang;
+
+    // FLAGS
+    const brFlag = "🇧🇷";
+    const usFlag = "🇺🇸";
+    // PORTUGUESE
+    const greetingsPT = "Olá, meu nome é Guilherme de Jesus Araujo Medeiros, o seu desenvolvedor full-stack.";
+    const titleAboutmePT = "Sobre mim";
+    const aboutmePT = "Eu sou formado em análise e desenvolvimento de sistemas pela Estácio e trabalho na área fazem 2 anos, mas eu ja obtive bastante experiência na área através de estágio por 3 anos.\nMeu maior foco de estudo é em full-stack, aprendendo tudo sobre o front-end e também pegando conhecimentos sobre APIs e SQL para o back-end.\n";
+    const proglangsPT = "Linguagens e afins";
+    const titlecontactPT = "Contato";
+    const footermsgPT = "Este site foi feito usando apenas HTML, CSS e JavaScript";
+    // ENGLISH
+    const greetingsEN = "Hello, my name is Guilherme de Jesus Araujo Medeiros, your full-stack developer.";
+    const titleAboutmeEN = "About me";
+    const aboutmeEN = "I have a degree in systems analysis and development from Estácio and have been working in the field for 2 years, but I have already gained a lot of experience in the area through an internship for 3 years.\nMy main focus of study is in full-stack, learning everything about front-end and also gaining knowledge about APIs and SQL for back-end.\n";
+    const proglangsEN = "Languages and related subjects";
+    const titlecontactEN = "Contact";
+    const footermsgEN = "This site was made using only HTML, CSS and JavaScript";
+
     
     if (lang === 'pt') {
-        const ptElements = document.getElementsByClassName('translatable-pt');
-        const enElements = document.getElementsByClassName('translatable-en');
-
-        for (let i = 0; i < ptElements.length; i++) {
-            ptElements[i].style.display = 'block';
-        }
-        for (let i = 0; i < enElements.length; i++) {
-            enElements[i].style.display = 'none';
-        }
-
+        document.getElementById('flag').innerText = brFlag;
+        document.getElementById('greetings').innerText = greetingsPT;
+        document.getElementById('titleAboutme').innerText = titleAboutmePT;
+        document.getElementById('aboutme').innerText = aboutmePT;
+        document.getElementById('proglangs').innerText = proglangsPT;
+        document.getElementById('contact').innerText = titlecontactPT;
+        document.getElementById('footermsg').innerText = footermsgPT;
+        
         document.documentElement.lang = 'pt';
     }
     if (lang === 'en') {
-        const ptElements = document.getElementsByClassName('translatable-pt');
-        const enElements = document.getElementsByClassName('translatable-en');
-        
-        for (let i = 0; i < ptElements.length; i++) {
-            ptElements[i].style.display = 'none';
-        }
-        for (let i = 0; i < enElements.length; i++) {
-            enElements[i].style.display = 'block';
-        }
+        document.getElementById('flag').innerText = usFlag;
+        document.getElementById('greetings').innerText = greetingsEN;
+        document.getElementById('titleAboutme').innerText = titleAboutmeEN;
+        document.getElementById('aboutme').innerText = aboutmeEN;
+        document.getElementById('proglangs').innerText = proglangsEN;
+        document.getElementById('contact').innerText = titlecontactEN;
+        document.getElementById('footermsg').innerText = footermsgEN;
 
         document.documentElement.lang = 'en';
     }
@@ -33,17 +48,18 @@ function translatePage(lang) {
 
 function toggleDropdown(toggle, open) {
     const dropdown = document.querySelector('.dropdown');
-    if (toggle) {
-        if (dropdown.style.display = 'block') {
+    if (toggle === true) {
+        if (dropdown.style.display === 'block') {
             dropdown.style.display = 'none';
         } else {
             dropdown.style.display = 'block';
         }
-    } 
-    if (open === false) {
-        dropdown.style.display = 'none';
     } else {
-        dropdown.style.display = 'block';
+        if (open === false) {
+            dropdown.style.display = 'none';
+        } else {
+            dropdown.style.display = 'block';
+        }
     }
 }
 
